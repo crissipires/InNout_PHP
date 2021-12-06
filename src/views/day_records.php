c8182e4c3fd8cad0cb3debc0472c16daecdb380a
crissipires <crissiano_pires@hotmail.com>
2021-12-06 16:43:57 -0300

    adicionando funcao pra titulo

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,11 +1,8 @@
 <main class="content">
-    <div class="content-title mb-4">
-        <i class="icon icofont-check-alt mr2"></i>
-        <div>
-            <h1>Registrar Ponto</h1>
-            <h2>Mantenha seu ponto consistente!</h2>
-        </div>
-    </div>
+    <?php
+        renderTitle("Registrar Ponto", 'Mantenha seu ponto consistente', 'icofont-check-alt');
+
+    ?>
     <div class="card">
         <div class="card-header">
             <h1>10 de janeiro de 2025</h1>
